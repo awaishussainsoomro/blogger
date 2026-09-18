b9b2c31891cf2f04354ac87bd60e37325ff6c236
awaishussainsoomro <awaishussainsmr0@gmail.com>
2026-09-19 00:46:41 +0500

    feat(ui): implement Header, Footer, and reusable form components (Button, Input)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import Logo from "../Logo";
+import Logo from "../index";
 function Footer() {
   return (
     <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
