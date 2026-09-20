a217116e1a5da4a010bf13809689662d2b62cdf8
awaishussainsoomro <awaishussainsmr0@gmail.com>
2026-09-20 11:37:17 +0500

    fix(ui): initialize options parameter as default empty array in Select component

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,32 +1,23 @@
-import React, { useId } from "react"
+import React, { useId } from "react";
 
-function Select({
-    options,
-    label,
-    className = "",
-    ...props
-    }, 
-    ref
-) {
-    const id = useId();
+function Select({ options = [], label, className = "", ...props }, ref) {
+  const id = useId();
   return (
     <div className="w-full">
-        {label && <label 
-        className="" 
-        htmlFor={id}></label>}
-        <select 
+      {label && <label className="" htmlFor={id}></label>}
+      <select
         ref={ref}
-        {...props} 
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} 
+        {...props}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         id={id}>
-            {options?.map((option) => (
-                <option key={option} value={option}>
-                    {option}
-                </option>
-            ))}
-        </select>
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
     </div>
-  )
+  );
 }
 
-export default React.forwardRef(Select)
+export default React.forwardRef(Select);
