f847375e77a958ba4ea760512a46f419c601059c
awaishussainsoomro <awaishussainsmr0@gmail.com>
2026-09-20 11:38:11 +0500

    fix(ui): resolve react-router import and component export syntax

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
-import { Container, Logo, LogoutBtn } from "../index";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Container, Logo, LogoutBtn } from '../index';
+import { Link, useNavigate } from 'react-router';
+import { useSelector } from 'react-redux';
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
