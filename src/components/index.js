16e2cb00be2312113bc52fd2d01ba311debe8129
awaishussainsoomro <awaishussainsmr0@gmail.com>
2026-09-21 13:59:16 +0500

    feat(ui): add Login component integrated with react-hook-form and Redux

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,21 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Login from "./Login";
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    PostCard,
+    Select,
+    Login
 }
