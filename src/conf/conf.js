f4bb1eef82b24ea4d43070ee074c57503d405af5
awaishussainsoomro <awaishussainsmr0@gmail.com>
2026-09-25 01:20:04 +0500

    update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,8 +1,8 @@
 const conf = {
     appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteTableID: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
     appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
